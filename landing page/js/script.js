d7bbb74f29f60a8908523f571972bf5644df3c80
Tiago Januário <tiagojdsa@gmail.com>
2021-02-14 15:06:14 -0300

    Update script.js

--- landing page/js/script.js
+++ landing page/js/script.js
@@ -2,8 +2,11 @@ function enviar(){
     let nome = document.getElementById('nomeid')
     let email = document.getElementById('emailid')
     let tel = document.getElementById('telid')
-    if (nome.value != "") {
-        
+    if (nome || email || tel == null){
+        alert('Preenche os campos Vazio')
+    }
+    else (nome.value != "") 
+
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
-}
+    
